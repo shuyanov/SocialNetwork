204a99298ea9083f7c0263d6c9a3d81c2a734046
sweet62 <maks_shuyanov@mail.ru>
2023-01-05 21:39:22 +0300

    FirstPushThisProject

---
+++ src/components/Profile.jsx
@@ -0,0 +1,14 @@
+const Profile = () => {
+    return (
+      <div className='content'>
+      <div>
+        <img src='https://gamemag.ru/images/cache/News/News167810/7ed34006a1-2_1390x600.jpg' />
+      </div>
+      <div>
+        <img src='https://i.pinimg.com/564x/a2/ba/ce/a2bacee80dab269f61447fd1405a4cba.jpg' />
+      </div>
+    </div>
+    )
+}
+
+export default Profile;
