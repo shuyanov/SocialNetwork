e9bf361f621a605d65d992790b1ec7e00274bf00
sweet62 <maks_shuyanov@mail.ru>
2023-01-08 20:35:56 +0300

    Add Dialog 22

--- src/components/Dialog/Dialog.jsx
+++ src/components/Dialog/Dialog.jsx
@@ -1,9 +1,35 @@
-import dealcss from './Dialog.module.css';
+import classes from './Dialog.module.css';
+import { NavLink } from "react-router-dom";
+
+const activeLink = ({isActive}) => isActive ? classes.active : classes.Dialog; 
 
 const Dialog = () => {
     return (
-        <div>
-            Dialog
+        <div className={classes.Dialogs}>
+            <div className={classes.DialogItems}>
+                <div className={classes.Dialog}>
+                    <NavLink to='/dialogs/1' className={activeLink}>Max</NavLink>
+                </div>
+                <div className={classes.Dialog}>
+                    <NavLink to='/dialogs/2' className={activeLink}> MaxClone1</NavLink>
+                </div>
+                <div className={classes.Dialog}>
+                    <NavLink to='/dialogs/3' className={activeLink}> MaxClone2</NavLink>
+                </div>
+                <div className={classes.Dialog}>
+                    <NavLink to='/dialogs/4' className={activeLink}> MaxClone3</NavLink>
+                </div>
+                <div className={classes.Dialog}>
+                    <NavLink to='/dialogs/5' className={activeLink}> MaxClone4</NavLink>
+                </div>
+            </div>
+            <div className={classes.MassagItems}>
+                <div className={classes.Massag}>Massag1</div>
+                <div className={classes.Massag}>Massag223</div>
+                <div className={classes.Massag}>Massag234</div>
+                <div className={classes.Massag}>Massag123</div>
+                <div className={classes.Massag}>Massag123</div>
+            </div>
         </div>
     )
 }
