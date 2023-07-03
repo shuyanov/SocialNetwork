2d50db16636acf5a15b8baf4e85af65f818981a6
Sweet <maks_shuyanov@mail.ru>
2023-07-03 22:22:01 +0300

    Feat: make disign

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,22 @@
-import FlexContainer from '../../Common/FlexContainer/FlexContainer';
-import HD from './Header.module.css';
+import FlexContainer from "../../Common/FlexContainer/FlexContainer";
+import style from "./Header.module.css";
 
 const Header = () => {
-    return (
-        <FlexContainer width={1440} height={100}>
-
-        </FlexContainer>
-    )
-}
+  return (
+    <FlexContainer
+      className={style.Header}
+      width={1440}
+      height={100}
+      justifyContent={"start"}
+      alignItems={"center"}
+    >
+      <div style={{ marginLeft: 105 }} className={style.Logo}></div>
+      <input style={{ marginLeft: 110 }} className={style.input}></input>
+      <button style={{ marginLeft: 50 }} className={style.button}></button>
+      <button style={{ marginLeft: 50 }} className={style.button}></button>
+      <button style={{ marginLeft: 525 }} className={style.button}></button>
+    </FlexContainer>
+  );
+};
 
 export default Header;
